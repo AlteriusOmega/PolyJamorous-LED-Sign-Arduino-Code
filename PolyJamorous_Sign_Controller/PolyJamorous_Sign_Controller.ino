81c62bb54eb281d7662459e1863969aa0c1f8773
AlteriusOmega <TheRealSamChaney@gmail.com>
2023-05-24 18:57:54 -0500

    Fixed the freezing issue! It was caused by sine Palette, renamed SinePaletteExplosion, which overloaded the LED strip onboard controller by flashing all LEDs simultaneously.  Renamed the file to remove version number as uing Git for version control now

--- PolyJamorous_Sign_Controller/PolyJamorous_Sign_Controller.ino
+++ PolyJamorous_Sign_Controller/PolyJamorous_Sign_Controller.ino
@@ -38,8 +38,7 @@ CRGB rawLeds[NUM_LEDS];
 CRGBSet leds(rawLeds, NUM_LEDS);
 
 
-struct Letter
-{
+struct Letter{
   CRGBSet top;
   CRGBSet bottom;
 };
@@ -70,11 +69,10 @@ Letter PolyJamorous[12] = {P,O1,L,Y,J,A,M,O2,R,O3,U,S};
 //////////////////////////////////////// FUNCTIONS /////////////////////////////////////////////////
 //////////////////////////////////////////////// ///////////////////////////////////////////////////
 
-// all color doLetterPalette(PolyJamorous[i], palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom] ): CloudColors_p LavaColors_p OceanColors_p ForestColors_p RainbowColors_p RainbowStripeColors_p PartyColors_p HeatColors_p
+// all color DoLetterPalette(PolyJamorous[i], palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom] ): CloudColors_p LavaColors_p OceanColors_p ForestColors_p RainbowColors_p RainbowStripeColors_p PartyColors_p HeatColors_p
 
 
-void doLetterPalette(struct Letter letter, CRGBPalette16 paletteTop, CRGBPalette16 paletteBottom, uint8_t index)
-{
+void DoLetterPalette(struct Letter letter, CRGBPalette16 paletteTop, CRGBPalette16 paletteBottom, uint8_t index){
   for (uint8_t x=0; x<sizeof(letter.top); x++){
     for (uint8_t y=0; y<sizeof(letter.bottom); y++){
       letter.top = ColorFromPalette(paletteTop, gPaletteIndex, 255); //palette, index, brightness
@@ -83,92 +81,81 @@ void doLetterPalette(struct Letter letter, CRGBPalette16 paletteTop, CRGBPalette
   }
 }
 
-void polyRhythm(uint8_t numer, uint8_t numerLeds[], uint8_t numerLength, uint8_t numerHue, uint8_t denom, uint8_t denomLeds[], uint8_t denomLength, uint8_t denomHue)
-{
+void PolyRhythm(uint8_t numer, uint8_t numerLeds[], uint8_t numerLength, uint8_t numerHue, uint8_t denom, uint8_t denomLeds[], uint8_t denomLength, uint8_t denomHue){
   fadeToBlackBy(leds, NUM_LEDS, 10);
   //Numerator
   EVERY_N_MILLISECONDS ( 60000 / (BPM / numer) / 2 ) 
   {
-    for(uint8_t i=0 ; i < numerLength ; i++)
-    {
-    leds[ numerLeds[i] ] = CHSV (numerHue, 255, 255); // ColorFromPalette(numerPalette, gPaletteIndex, 255);
+    for(uint8_t i=0 ; i < numerLength ; i++){
+      leds[ numerLeds[i] ] = CHSV (numerHue, 255, 255); // ColorFromPalette(numerPalette, gPaletteIndex, 255);
     }
   }
   //Denominator
-  EVERY_N_MILLISECONDS ( 60000/(BPM / denom) / 2) 
-  {
-    for(uint8_t i=0 ; i < denomLength ; i++)
-    {
-    leds[ denomLeds[i] ] =  CHSV (denomHue, 255, 255); // ColorFromPalette(denomPalette,  gPaletteIndex, 255); 
+  EVERY_N_MILLISECONDS ( 60000/(BPM / denom) / 2) {
+    for(uint8_t i=0 ; i < denomLength ; i++){
+      leds[ denomLeds[i] ] =  CHSV (denomHue, 255, 255); // ColorFromPalette(denomPalette,  gPaletteIndex, 255); 
     }
   }
 }
 
-void polyRhythm_2_3()
-{
-  polyRhythm (2, Top,sizeof(Top), 171, 3, Bottom, sizeof(Bottom), 0);
+void PolyRhythm_2_3(){
+  PolyRhythm (2, Top,sizeof(Top), 171, 3, Bottom, sizeof(Bottom), 0);
 }
 
-void polyRhythm_3_4()
-{
-  polyRhythm (3, Top,sizeof(Top), 85, 4, Bottom, sizeof(Bottom), 200);
+void PolyRhythm_3_4(){
+  PolyRhythm (3, Top,sizeof(Top), 85, 4, Bottom, sizeof(Bottom), 200);
 }
 
-void POLYJAMOROUS_SIMPLE()
-{
+void POLYJAMOROUS_SIMPLE(){
       leds.fadeToBlackBy(1);
-      doLetterPalette(PolyJamorous[gCurrentLetter], palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],gPaletteIndex);
-      doLetterPalette(PolyJamorous[ (gCurrentLetter+6) % 12 ], palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],gPaletteIndex);
+      DoLetterPalette(PolyJamorous[gCurrentLetter], palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],gPaletteIndex);
+      DoLetterPalette(PolyJamorous[ (gCurrentLetter+6) % 12 ], palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],gPaletteIndex);
       EVERY_N_MILLISECONDS (60000/BPM) {gCurrentLetter = ( gCurrentLetter+1 ) % ( sizeof(PolyJamorous) / sizeof(PolyJamorous[0]));}
 }
 
-void POLYJAMOROUS_CHANT()
-{
+void POLYJAMOROUS_CHANT(){
 
-  for(uint8_t i = 0 ; i < 4 ; i++)
-  {
+  for(uint8_t i = 0 ; i < 4 ; i++){
     FastLED.clear();
     FastLED.show(); 
-    doLetterPalette(PolyJamorous[i], palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom] ,gPaletteIndex);
+    DoLetterPalette(PolyJamorous[i], palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom] ,gPaletteIndex);
     FastLED.delay(60000/BPM); // quarter notes
   }
   
-  for(uint8_t i = 4 ; i < 12 ; i++)
-  {
+  for(uint8_t i = 4 ; i < 12 ; i++){
     FastLED.clear();
        FastLED.show(); 
-    doLetterPalette(PolyJamorous[i], palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom] ,gPaletteIndex);
+    DoLetterPalette(PolyJamorous[i], palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom] ,gPaletteIndex);
     FastLED.delay((60000/BPM)/2); // 8th notes
   }
 }
 
-void POLY_JAM_R_US_2()
-{
+void POLY_JAM_R_US_2(){
     uint8_t sine = beatsin16( 50, 0, 5 ); //BEAT (bpm), MIN, MAX
     uint8_t index = gPaletteIndexFunc + sine;
     leds.fadeToBlackBy(10);
     EVERY_N_MILLISECONDS (60000/BPM) {gCurrentLetter = ( gCurrentLetter+1 ) % 4;}
     switch (gCurrentLetter) {
     case 0: //POLY
-    doLetterPalette(P, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
-    doLetterPalette(O1, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
-    doLetterPalette(L, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
-    doLetterPalette(Y, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
+    DoLetterPalette(P, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
+    DoLetterPalette(O1, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
+    DoLetterPalette(L, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
+    DoLetterPalette(Y, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
     break;
     
     case 1: //JAM
-    doLetterPalette(J, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
-    doLetterPalette(A, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
-    doLetterPalette(M, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
+    DoLetterPalette(J, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
+    DoLetterPalette(A, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
+    DoLetterPalette(M, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
     break;
 
     case 2: //R
-    doLetterPalette(R, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
+    DoLetterPalette(R, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
     break;
 
     case 3: //US
-    doLetterPalette(U, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
-    doLetterPalette(S, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
+    DoLetterPalette(U, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
+    DoLetterPalette(S, palettes[gCurrentPaletteTop], palettes[gCurrentPaletteBottom],index);
     break;
 
     default: //if gCurrentLetter isn't any of these cases, set it to 0
@@ -177,10 +164,8 @@ void POLY_JAM_R_US_2()
     }
 }
 
-void RainbowCircle()
-{   
-  for(uint8_t i=0 ; i<NUM_LEDS ; i++)
-  {
+void RainbowCircle(){  
+  for(uint8_t i=0 ; i<NUM_LEDS ; i++){
       fadeToBlackBy(leds, NUM_LEDS, 10);
       leds[i] = CHSV(rando,255,255);
       FastLED.delay(30);
@@ -188,51 +173,36 @@ void RainbowCircle()
   }
 }
 
-void RainbowCircleDouble()
-{
+void RainbowCircleDouble(){
     fadeToBlackBy(leds, NUM_LEDS, 10);
     leds[gCCW] = CHSV(gHue*6+150,255,255);
     leds[gCW] = CHSV(gHue*6,255,255);
-    if (gCCW == 0)
-    {
+    if (gCCW == 0){
       gCCW = 61;
     }
-    else
-    {
+    else{
         gCCW--; 
     }
 
-    if (gCW == 61)
-    {
+    if (gCW == 61){
       gCW = 0;
     }
-    else
-    {
+    else{
         gCW++; 
     }
     FastLED.delay(30);
 }
 
-void matrix()
-{
+void Matrix(){
   uint8_t currentLetter = random(0,11);
   EVERY_N_MILLISECONDS (400){currentLetter = random(0,11);}
   leds.fadeToBlackBy(10);
-  doLetterPalette(PolyJamorous[currentLetter], ForestColors_p, ForestColors_p, gPaletteIndex);
+  DoLetterPalette(PolyJamorous[currentLetter], ForestColors_p, ForestColors_p, gPaletteIndex);
 }
 
-void sineCircle() //TODO remove function
-{
-  uint8_t pos = beatsin16( 50, -2, 1 ); //BEAT (bpm), MIN, MAX
-  fadeToBlackBy(leds, NUM_LEDS, 10);
-  leds[(gCounterFunc+pos) % NUM_LEDS] = CHSV(gHueFunc,255,255);
-  FastLED.delay(60);
-  gHueFunc++;
-  gCounterFunc++;
-}
 
-void sinePalette()
-{
+void SinePaletteExplosion(){
+  // Causes the LED strip to freeze! Flashing all LEDs at once causes the controller to be overloaded and lock up, do not use!
   fadeToBlackBy(leds, NUM_LEDS, 20);
   uint8_t sine = beatsin16(BPM, 0, 10);
   uint8_t index = gPaletteIndexFunc + sine;
@@ -241,8 +211,7 @@ void sinePalette()
   leds[ random16(NUM_LEDS) ] += ColorFromPalette(palettes [gCurrentPaletteTop], random16(255), 255);
 }
 
-void Bounce()
-{
+void Bounce(){
   uint8_t pos = beatsin16( BPM/2, 0, sizeof(Top) -1 );
   leds.fadeToBlackBy(10);
   leds[ Top[pos] ] += CHSV(gHue*10,255,255);
@@ -251,8 +220,7 @@ void Bounce()
   leds[ Bottom[pos] ] += CHSV(gHue*10,255,255);
 }
 
-void ZoomIn()
-{
+void ZoomIn(){
     fadeToBlackBy(leds, NUM_LEDS, 25);
     uint8_t center = 15;
     uint8_t topCenter = sizeof(Top)/2 -1;
@@ -266,31 +234,32 @@ void ZoomIn()
     leds[ Bottom[bottomCenter - gCounter - 5] ]  = CHSV(gHueFunc,255,255);
   
     //first need to rein in gCounter, should only happen once
-    if (gCounter > width)
-    {
+    if (gCounter > width){
       gCounter = 0;
     }
     
     //now set the pattern. gCounter is incremented until it is equal to width
-    if (gCounter == width)
-    {
+    if (gCounter == width){
       gCounter = 0;
       gHueFunc = gHueFunc + 8; //change color drastically between zooms
     }
-    else
-    {
+    else{
       gCounter++;
     }
     gHueFunc = gHueFunc + 5; //change color slightly between LEDS
     FastLED.delay(60000/BPM/width);
 }
 
-void ResetLEDs() // Sets all LEDs to black for a short time to try to fix the freezing issue
-{
+void ResetLEDs(){ // Sets all LEDs to black for a short time to try to fix the freezing issue
   for(int i=0; i<NUM_LEDS; i++){
     leds[i] = CRGB::Black;
   }
   delay(5000); // Wait 5 seconds
+}
+
+void Amor(){
+  DoLetterPalette()
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -305,17 +274,15 @@ void setup() {
 
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
-  ResetLEDs();
-//  wdt_enable(WDTO_2S);
+//  wdt_enable(WDTO_2S); // Watch dog timer enable
 }
 
 
 ////////////////////////////////////// POST-FUNCTION DECLARATIONS //////////////////////////////////
 typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = {polyRhythm_2_3, POLY_JAM_R_US_2, ZoomIn, polyRhythm_3_4, Bounce, matrix,  sinePalette, POLYJAMOROUS_SIMPLE, RainbowCircle, RainbowCircleDouble, ResetLEDs}; //POLYJAMOROUS_SIMPLE
+SimplePatternList gPatterns = {PolyRhythm_2_3, POLY_JAM_R_US_2, ZoomIn, PolyRhythm_3_4, Bounce, Matrix, POLYJAMOROUS_SIMPLE, RainbowCircle, RainbowCircleDouble, ResetLEDs}; //POLYJAMOROUS_SIMPLE
 
-void nextPattern()
-{
+void nextPattern(){
   // add one to the current pattern number, and wrap around at the end
   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % (sizeof(gPatterns)/sizeof(gPatterns[0]) );
 }
@@ -327,11 +294,11 @@ void loop() {
     FastLED.show();
     FastLED.delay(1000/FRAMES_PER_SECOND); 
     EVERY_N_MILLISECONDS( 51 ) { gHue++; }; // slowly cycle the "base color" through the rainbow
-    EVERY_N_MILLISECONDS( 2 ) { gPaletteIndex++; }; // slowly cycle the "base color" through the rainbow
+    EVERY_N_MILLISECONDS( 2 ) { gPaletteIndex++; }; // slowly cycle the palette index through the rainbow
     EVERY_N_SECONDS (gPaletteTopTime) {gCurrentPaletteTop = (gCurrentPaletteTop + 1) % (sizeof(palettes) / sizeof(palettes[0]) );}
     EVERY_N_SECONDS (gPaletteBottomTime) {gCurrentPaletteBottom = (gCurrentPaletteBottom + 1) % (sizeof(palettes) / sizeof(palettes[0]) );}
     EVERY_N_SECONDS( 19 ) { nextPattern(); } // change patterns periodically
-//    wdt_reset();
+//    wdt_reset(); // Watch dog timer reset
     gPatterns[gCurrentPatternNumber]();
 
 
